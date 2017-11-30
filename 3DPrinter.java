a77f26636370bb4f6e965799dc1e45374fe80b78
Reshab Chhabra <33813644+rechhabra@users.noreply.github.com>
2017-11-30 18:34:05 -0500

    Create 3DPrinter.java

---
+++ 3DPrinter.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class 3DPrinter{
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        System.out.println(1+(int)Math.ceil(Math.log(kb.nextDouble())/Math.log(2)));
+    }
+}
