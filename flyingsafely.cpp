38e513b2ec4b5f20cfddc266c1ed51a3c1e2c724
Reshab Chhabra <33813644+rechhabra@users.noreply.github.com>
2017-12-31 01:01:18 -0500

    Create flyingsafely.cpp

---
+++ flyingsafely.cpp
@@ -0,0 +1,27 @@
+#include <iostream>//std
+#include <cstring>//strlen
+#include <algorithm>//math stuff
+#include <math.h>//pow,ceil,sin,cos,etc.
+#include <vector>//vector
+#include <string>//string opers
+#include <stack> //stack list
+#include <sstream> //split asst., string to int
+#include <stdio.h>//extra
+#include <iomanip>//round n digit: cout<<setprecision(n)<<fixed<<
+#include <bits/stdc++.h>//permutations
+using namespace std; //removes use of std::
+const long double PI= 3.141592653589793238L;
+int main(){
+  int tc;
+  cin>>tc;
+  for (int i = 0; i<tc; i++){
+    int numP,numC;
+    scanf("%d %d",&numC,&numP);
+    for (int j = 0; j<numP; j++){
+      int u1,u2;
+      scanf("%d %d",&u1,&u1);
+    }
+    cout<<numC-1<<endl;
+  }
+  return 0;
+}
