6d4db26d050f3390bfbcc5c1d48dc6baab65f9f4
Reshab Chhabra <33813644+rechhabra@users.noreply.github.com>
2018-01-04 17:01:57 -0500

    Create tetration.cpp

---
+++ tetration.cpp
@@ -0,0 +1,19 @@
+#include <iostream>//std
+#include <cstring>//strlen
+#include <algorithm>//math stuff
+#include <math.h>//pow,ceil,sin,cos,etc.
+#include <vector>//vector
+#include <string>//string opers
+#include <stack> //stack list
+#include <sstream> //split asst., string to int
+#include <stdio.h>//extra
+#include <iomanip>//round n digit: cout<<setprecision(n)<<fixed<<
+#include <bits/stdc++.h>//permutations
+using namespace std; //removes use of 
+const long double PI= 3.141592653589793238L;
+int main(){
+  double x;
+  cin>>x;
+  cout<<setprecision(6)<<fixed<<pow(x,1/x)<<endl;
+  return 0;
+}
