d9dd06cbf0b97ad95c3b1acea3e66025deb7df2e
Reshab Chhabra <33813644+rechhabra@users.noreply.github.com>
2018-01-10 04:13:41 -0500

    Create quickestimates.cpp

---
+++ quickestimates.cpp
@@ -0,0 +1,26 @@
+#include <iostream>//std
+#include <cstring>//strlen
+#include <algorithm>//math stuff
+#include <math.h>//pow,ceil,sin,cos,etc.
+#include <vector>//vector
+#include <string>//string opers
+#include <stack> //stack list
+#include <sstream> //split asst., string to ll
+#include <stdio.h>//extra
+#include <iomanip>//round n digit: cout<<setprecision(n)<<fixed<<
+#include <bits/stdc++.h>//permutations
+#include <unordered_set>//sets like python
+#include <set>
+using namespace std; //removes use of 
+typedef long long ll;
+const long double PI= 3.141592653589793238L;
+int main(){
+  int tc;
+  cin>>tc;
+  while (tc--){
+    char v[101];
+    cin>>v;
+    cout<<strlen(v)<<endl;
+  }
+  return 0;
+}
