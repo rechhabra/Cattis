d6484fd2c85462568056e8506d609df9e06ad6ee
Reshab Chhabra <33813644+rechhabra@users.noreply.github.com>
2018-03-02 17:56:30 -0500

    Create jewelrybox.rb

---
+++ jewelrybox.rb
@@ -0,0 +1,11 @@
+tc = gets.to_i
+(1..tc).each do |_|
+  x,y = gets.split(" ").map{|x| x.to_r}
+  a = (x+y)/6
+  b = (Math.sqrt(x*x + y*y - x*y))/6
+  amnt = a+b
+  if not amnt <[x,y].min/2 and amnt>0
+    amnt = a-b
+  end
+  puts(amnt*(x-2*amnt)*(y-2*amnt))
+end
