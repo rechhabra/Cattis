c52f9514d3410c4fbb826d10d73341e238b2c5ad
Reshab Chhabra <33813644+rechhabra@users.noreply.github.com>
2018-05-07 18:42:41 -0400

    Create flexible.py

---
+++ flexible.py
@@ -0,0 +1,15 @@
+#Reshab Chhabra
+#Time: 0.02s
+from itertools import combinations
+W,L = map(int, input().split())
+vals = [0,W]+[int(x) for x in input().split()]
+ans = set()
+ans.add(0)
+combos = combinations(vals,2)
+for group in combos:
+  ans.add(abs(group[0]-W))
+  ans.add(abs(group[0]-0))
+  ans.add(abs(group[0]-W))
+  ans.add(abs(group[0]-0))
+  ans.add(abs(group[0]-group[1]))
+print(*sorted(list(ans))[1:])
